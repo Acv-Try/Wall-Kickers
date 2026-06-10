b29eaf3a949b915b0fc4e2d27ba981d980ef9247
David Petrosyan <davit.petrosyan3.y@gmailc.om>
2026-06-10 23:18:55 +0400

    -- corrected animations

--- Assets/DavGame/CameraFollowing1.cs
+++ Assets/DavGame/CameraFollowing1.cs
@@ -21,7 +21,7 @@ public class CameraFollowing1 : MonoBehaviour
         }
         else
         {
-            NewPosition = new Vector3(center.x, Target.position.y > transform.position.y ? Target.position.y : transform.position.y, -10); // Update the camera's position to follow the player
+            NewPosition = new Vector3(center.x, (Target.position.y > transform.position.y ? Target.position.y : transform.position.y ), -10); // Update the camera's position to follow the player
         }
         transform.position = Vector3.Lerp(transform.position, NewPosition, Time.deltaTime * 5f);
     }
