af61298b7f126dceac3d5c5eedebcec6315218ed
David Petrosyan <davit.petrosyan3.y@gmailc.om>
2026-06-13 16:29:34 +0400

    -- Added Camera Smooth movement

--- Assets/DavGame/CameraFollowing1.cs
+++ Assets/DavGame/CameraFollowing1.cs
@@ -3,26 +3,55 @@ using UnityEngine;
 public class CameraFollowing1 : MonoBehaviour
 {
     [SerializeField] private Transform Target;
-    [SerializeField] private float Xoffset;
-    [SerializeField] private Vector3 cameraOffset;
-    [SerializeField] private Vector3 center;
-    Vector3 NewPosition;
 
+    [SerializeField] private float Xoffset;
+    [SerializeField] private float YOffset;
+
+    [SerializeField] private Vector3 center;
+    [SerializeField] private float speed;
+    
+    Vector3 NewPosition;
+    
     private void Start()
     {
-        transform.position = Target.position + cameraOffset;
+        transform.position = new Vector3(center.x, center.y + YOffset, -10);
+        Debug.DrawLine(
+            new Vector3(center.x + Xoffset, center.y - 100f, center.z),
+            new Vector3(center.x + Xoffset, center.y + 100f, center.z),
+            Color.red,
+            10f
+        );
+
+        Debug.DrawLine(
+            new Vector3(center.x - Xoffset, center.y - 100f, center.z),
+            new Vector3(center.x - Xoffset, center.y + 100f, center.z),
+            Color.red,
+            10f
+        );
     }
 
     void Update()
     {
-        if (Target.position.x >= center.x + Xoffset || Target.position.x <= center.x - Xoffset)
-        {
-            NewPosition = new Vector3(Target.position.x, Target.position.y > transform.position.y ? Target.position.y : transform.position.y, -10); // Update the camera's position to follow the player
-        }
-        else
-        {
-            NewPosition = new Vector3(center.x, (Target.position.y > transform.position.y ? Target.position.y : transform.position.y ), -10); // Update the camera's position to follow the player
-        }
-        transform.position = Vector3.Lerp(transform.position, NewPosition, Time.deltaTime * 5f);
+        float y = Mathf.Max(transform.position.y, Target.position.y + YOffset);
+
+        float x = center.x;
+
+        if (Target.position.x > center.x + Xoffset)
+            x = Target.position.x - Xoffset/3;
+        else if (Target.position.x < center.x - Xoffset)
+            x = Target.position.x + Xoffset / 3;
+
+        NewPosition = new Vector3(x, y, -10);
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            NewPosition,
+            speed * Time.deltaTime
+        );
+    }
+
+    public void UpdateCenter(Vector3 newPos)
+    {
+        center = newPos;
     }
 }
