53e3e2ebfe183468d4eda17e84e23d1a9246d1e7
Armo <armohovnikyan@gmail.com>
2026-07-09 12:32:23 +0400

    Ui Improving

--- Assets/_Game/Scripts/Events/GameEvents.cs
+++ Assets/_Game/Scripts/Events/GameEvents.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 public class GameEvents : MonoBehaviour
 {
     public static event Action OnGameStart;
-    public static event Action OnPouseButtonClick;
+    public static event Action OnGameEnd;
+    public static event Action OnPauseButtonClick;
+    public static event Action OnPlayButtonClick;
 
     public static void RaiseOnGameStart()
     {
         OnGameStart?.Invoke();
     }
 
-    public static void RaiseOnPouseButtonClick()
+    public static void RaiseOnGameEnd()
     {
-        OnPouseButtonClick?.Invoke();
+        OnGameEnd?.Invoke();
+    }
+
+    public static void RaiseOnPauseButtonClick()
+    {
+        OnPauseButtonClick?.Invoke();
+    }
+
+    public static void RaiseOnPlayButtonClick()
+    {
+        OnPlayButtonClick?.Invoke();
     }
 }
