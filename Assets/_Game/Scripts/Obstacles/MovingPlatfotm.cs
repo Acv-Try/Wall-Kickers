4c938ff69c3ee3f44b566ed13f2d2ef2e6698eba
Frone <galstanartur2009@gmail.com>
2026-07-09 15:54:53 +0400

    twolevels

--- Assets/_Game/Scripts/Obstacles/MovingPlatfotm.cs
+++ Assets/_Game/Scripts/Obstacles/MovingPlatfotm.cs
@@ -4,45 +4,37 @@
 //{
 //    Rigidbody2D rb;
 //    BoxCollider2D col;
-
 //    void Start()
 //    {
 //        col = GetComponent<BoxCollider2D>();
 //    }
-//    public override void Touched(Player player)
+//    public override void Touched(PlayerController player)
 //    {
 //        player.transform.SetParent(transform);
 //        rb = player.rb;
 //    }
-
-//    public override void Left(Player player)
+//    public override void Left(PlayerController player)
 //    {
 //        player.transform.SetParent(null);
 //        rb = null;
 //        col.enabled = false;
 //        StartCoroutine(ResetColider());
 //    }
-
 //    IEnumerator ResetColider()
 //    {
 //        yield return new WaitForSeconds(0.1f);
 //         col.enabled = true;
 //    }
-
 //    void FixedUpdate()
 //    {
 //         PlatformVelocity = ((Vector2)transform.position - LastPosition) / Time.fixedDeltaTime;
-
 //        LastPosition = transform.position;
-
 //        if(rb != null)
 //        {
 //            rb.position += PlatformVelocity * Time.fixedDeltaTime;
 //            rb.linearVelocity = Vector2.zero;
 //        }
 //    }
-
 //    Vector2 LastPosition;
 //    Vector2 PlatformVelocity;
-
 //}
